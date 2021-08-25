a66f595d5ef48e3674315e5a7f5bebcae8812f4d
k.aseev <k.aseev@okko.tv>
2021-08-25 11:05:49 +0300

    add udp top scane

--- scane_all.sh
+++ scane_all.sh
@@ -7,11 +7,26 @@ echo
 while read line;do
     echo "$line"
     mkdir $Path/$line
-    sshpass -p "$password" sudo nmap -A -sS -Pn -p- $line -oN $Path/$line/nmap_out --min-rate=3000
+    sshpass -p "$password" sudo nmap -A -sS -Pn -p- $line -oN $Path/$line/nmap_out --min-rate=5000
 done < $FILE
 while read line;do
-    if [ $(cat $Path/$line/nmap_out | grep "80/tcp open" | wc -m) -gt 0 ];then 
-        nikto -h $line -o $Path/$line/nikto_out -Format txt &
-        dirb http://$line -o $Path/$line/dirb_out &
+    if [ $(cat $Path/$line/nmap_out | grep '80/tcp[[:blank:]]*open' | wc -m) -gt 0 ];then 
+        nikto -h $line -o $Path/$line/nikto_80_out -Format txt &
+        dirb http://$line -o $Path/$line/dirb_80_out &
     fi 
+done < $FILE
+while read line;do
+    if [ $(cat $Path/$line/nmap_out | grep '443/tcp[[:blank:]]*open' | wc -m) -gt 0 ];then 
+        nikto -h $line -o $Path/$line/nikto_80_out -Format txt &
+        dirb http://$line -o $Path/$line/dirb_80_out &
+    fi 
+done < $FILE
+while read line;do
+    if [ $(cat $Path/$line/nmap_out | grep '445/tcp[[:blank:]]*open' | wc -m) -gt 0 ];then 
+        nmap --script=smb* -p445 $line -o $Path/$line/smb_scane_out &
+    fi 
+done < $FILE
+while read line;do
+    mkdir $Path/$line
+    nmap nmap -sU --top-ports 1000 $line -oN $Path/$line/nmap_top_udp_out --min-rate=5000
 done < $FILE
