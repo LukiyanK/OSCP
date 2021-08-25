b450223da07f829d0aa4ddc809362d10b1595e16
k.aseev <k.aseev@okko.tv>
2021-08-25 10:54:16 +0300

    add nikto and dirb

--- scane_all.sh
+++ scane_all.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 FILE=$1
+Path=$2
+echo -n SUDO Password: 
+read -s password
+echo
 while read line;do
     echo "$line"
-done << $FILE
+    mkdir $Path/$line
+    sshpass -p "$password" sudo nmap -A -sS -Pn -p- $line -oN $Path/$line/nmap_out --min-rate=3000
+done < $FILE
+while read line;do
+    if [ $(cat $Path/$line/nmap_out | grep "80/tcp open" | wc -m) -gt 0 ];then 
+        nikto -h $line -o $Path/$line/nikto_out -Format txt &
+    fi 
+done < $FILE
+while read line;do
+    if [ $(cat $Path/$line/nmap_out | grep "80/tcp open" | wc -m) -gt 0 ];then 
+        dirb http://$line -o $Path/$line/dirb_out &
+    fi 
+done < $FILE
